aa4f7c56723b7e2055e837093993b9c193016ce4
Logan Hart <2loganhart@gmail.com>
2018-09-19 17:18:59 -0700

    implement PUT request to json-server to save updates to individual todos

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -23,6 +23,7 @@ import {
 import {
   loadTodos,
   createTodo,
+  saveTodo,
 } from './lib/todoService';
 
 class App extends Component {
@@ -49,11 +50,15 @@ class App extends Component {
   }
 
   handleToggle = (id) => {
-    const getUpdatedTodos = pipe(findById, toggleTodo,partial(updateTodo, this.state.todos));
-    const updatedTodos = getUpdatedTodos(id, this.state.todos);
+    const getToggledTodo = pipe(findById, toggleTodo);
+    const updated = getToggledTodo(id, this.state.todos);
+    const getUpdatedTodos = partial(updateTodo, this.state.todos);
+    const updatedTodos = getUpdatedTodos(updated);
     this.setState({
       todos: updatedTodos,
     });
+    saveTodo(updated)
+      .then(() => this.showTempMessage('Todo Updated'));
   }
 
   handleSubmit = (event) => {
