33c331d18d0ed6550b148cdfb39e85eeb2405ca7
Logan Hart <2loganhart@gmail.com>
2018-09-20 17:15:35 -0700

    add redux to project, implement initial reducers and actions, only using redux, not react-redux atm

--- todo-react-redux/src/components/TodoForm.js
+++ todo-react-redux/src/components/TodoForm.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default (props) => (
-  <form>
-    <input type="text" />
-  </form>
-);
+export default (props) => {
+  const {currentTodo, changeCurrent} = props;
+  const handleInputChange = (event) => {
+    const val = event.target.value;
+    changeCurrent(val);
+  }
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={currentTodo}
+      />
+    </form>
+  );
+}
