9b673bca5d2e473afb4f2486d49e9ad73aa994c8
Logan Hart <2loganhart@gmail.com>
2018-09-24 14:41:59 -0700

    Add redux-devtools to project, hook up to store

--- todo-react-redux/src/store.js
+++ todo-react-redux/src/store.js
@@ -2,10 +2,13 @@ import {
   createStore,
   applyMiddleware,
 } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducers/todo';
 
 export default createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 );
