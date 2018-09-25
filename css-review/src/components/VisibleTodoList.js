e98d446df9bd534cb0271cd5b604e5de42f8e890
Logan Hart <2loganhart@gmail.com>
2018-09-24 19:34:29 -0700

    Initial commit: refactor code / file structure from https://jsbin.com/yizosud/13/edit?js,output - adjusted for create react-app

---
+++ css-review/src/components/VisibleTodoList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getVisibleTodos } from '../reducers/getVisibleTodos';
+import { toggleTodo } from '../actions';
+
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration:
+        completed ?
+          'line-through' :
+          'none'
+    }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+const mapStateToTodoListProps = (
+  state
+) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
+    )
+  };
+};
+
+const mapDispatchToTodoListProps = (
+  dispatch
+) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodo(id));
+    }
+  };
+};
+
+export default connect(
+  mapStateToTodoListProps,
+  mapDispatchToTodoListProps
+)(TodoList);
