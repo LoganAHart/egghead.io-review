329740d9e7b512001d1b744514a3c76914913a47
Logan Hart <2loganhart@gmail.com>
2018-09-25 18:37:29 -0700

    Refactor express server, add put and delete routes, refactor users.hbs to include more info on users -- following course

--- express-review/index.js
+++ express-review/index.js
@@ -1,34 +1,80 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 const engines = require('consolidate');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const users = []
+const getUserFilePath = (username) => {
+  return path.join(__dirname, 'users', username) + '.json';
+}
 
-fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
-  if (err) { throw err; }
-  JSON.parse(data).forEach((user) => {
-    user.name.full = _.startCase(`${user.name.first} ${user.name.last}`)
-    users.push(user)
+const getUser = (username) => {
+  const user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}));
+  user.name.full = _.startCase(`${user.name.first} ${user.name.last}`)
+  _.keys(user.location).forEach((key) => {
+    user.location[key] = _.startCase(user.location[key])
   });
-});
+  return user;
+}
+
+const saveUser = (username, data) => {
+  const fp = getUserFilePath(username);
+  fs.unlinkSync(fp); // delete the file
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'});
+}
 
 app.engine('hbs', engines.handlebars);
 
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-app.use('/profilepics', express.static('images'))
+app.use('/profilepics', express.static('images'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/favicon.ico', (req, res) => {
+  res.end();
+});
 
 app.get('/', (req, res) => {
-  res.render('index', { users: users });
+  const users = [];
+  fs.readdir('users', (err, files) => {
+    files.forEach((file) => {
+      fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, (err, data) => {
+        const user = JSON.parse(data);
+        user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
+        users.push(user);
+        if (users.length === files.length) {
+          res.render('index', { users: users });
+        }
+      });
+    });
+  });
 });
 
 app.get('/:username', (req, res) => {
   const username = req.params.username;
-  res.render('user', { username: username });
+  const user = getUser(username);
+  res.render('user', {
+    user: user,
+    address: user.location
+  });
+});
+
+app.put('/:username', (req, res) => {
+  const username = req.params.username;
+  const user = getUser(username);
+  user.location = req.body;
+  saveUser(username, user);
+  res.end();
+});
+
+app.delete('/:username', (req, res) => {
+  const fp = getUserFilePath(req.params.username);
+  fs.unlinkSync(fp); // delete the file
+  res.sendStatus(200);
 });
 
 const server = app.listen(3000, () => {
